473f48038c0e7e75bec8817d9cbceb0b085d9d6c
Swaraj Farande <125743998+Swaraj131097@users.noreply.github.com>
2023-03-16 18:59:58 +0530

    Add files via upload

---
+++ positive-negative.py
@@ -0,0 +1,14 @@
+# take input from user
+num1 = int(input())
+num2 = int(input())
+
+# add the two numbers
+x = num1 + num2
+
+# check if x is positive or negative and print the result
+if x > 0:
+    print("positive")
+elif x < 0:
+    print("negative")
+else:
+    print("x is zero")
